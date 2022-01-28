a9df41914ada1d0be88fea549612d93f1862a6eb
Blueishere <84488513+hoangtuan30124@users.noreply.github.com>
2022-01-28 21:35:47 +0700

    Update RandomLink.js

--- RandomLink.js
+++ RandomLink.js
@@ -2,7 +2,7 @@
 var sites = {
     "rickroll": "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
     "winner": "prizes/winner.html",
-    "recursive": "?",
+    "recursive": "#",
     "comeback": "https://www.facebook.com/LPECpage"
 };
 const range = 5000;
